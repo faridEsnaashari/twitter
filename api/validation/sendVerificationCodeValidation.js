15040327e31cbaedc640fd62b9a68c3c15ea436f
kk <kk>
2020-09-08 15:24:02 +0430

    also check for currect phone number

--- api/validation/sendVerificationCodeValidation.js
+++ api/validation/sendVerificationCodeValidation.js
@@ -1,10 +1,15 @@
-const {isString, isUndefinedOrNull} = require('../tools/tools');
+const {isString, isUndefinedOrNull, isPhoneNumber} = require('../tools/tools');
 
 function checkValidation(req, res){
     if(isUndefinedOrNull(req.query.phonenumber)){
+        console.log("un");
         return false;
     }
     if(!isString(req.query.phonenumber)){
+        console.log("st");
+        return false;
+    }
+    if(!isPhoneNumber(req.query.phonenumber)){
         return false;
     }
     return true;
