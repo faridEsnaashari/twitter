b9ca882bdc8c770b25c3ae65ba7998d86bbce367
kk <kk>
2020-09-08 14:55:06 +0430

    check validation of request parameters

--- api/routes/sendVerificationCode.js
+++ api/routes/sendVerificationCode.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const sendVerificationCodeHandler = require('../controllers/sendVerificationCode/get');
+const checkValidation = require('../validation/sendVerificationCodeValidation');
+
 router.get('/', (req, res, next) => {
-    sendVerificationCodeHandler();
+    checkValidation(req, res);
+    sendVerificationCodeHandler(req, res);
 });
 
 module.exports = router;
