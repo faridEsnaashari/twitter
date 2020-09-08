5ae3e302153b8c03fb1caadfa2346ed68c0394e1
kk <kk>
2020-09-08 13:32:39 +0430

    add send sms functionality with sms-ir

--- api/controllers/sendVerificationCode/get.js
+++ api/controllers/sendVerificationCode/get.js
@@ -1,5 +1,15 @@
-function get(){
+const {Token,VerificationCode} = require('sms-ir')
+const token = new Token();
+const verificationCode = new VerificationCode();
+const {APIKEY} = require('../../config');
+const {SECRETKEY} = require('../../config');
 
+async function get(){
+    //get number
+    //get rand
+    //insert in database
+    const tokenResult = await token.get(APIKEY, SECRETKEY);
+    const verificationResult = await verificationCode.send(tokenResult, '09140466901', '11111');
 }
 
 module.exports = get;
