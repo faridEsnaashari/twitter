c3d1de48cbbe015caa2c1af84732f202190127c3
kk <kk>
2020-09-08 13:19:34 +0430

    add wrong route and sendverification route

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const mysql = require('mysql');
 const connection = require('./api/connection');
+
+const sendVerificationCodeRoute = require('./api/routes/sendVerificationCode');
 
 const app = express();
 
+app.use('/sendvrificationcode', sendVerificationCodeRoute);
+
 app.use((req, res) => {
+    const responseJson = {
+        message : "wrong route or method"
+    };
+    res.status(404).json(responseJson);
 });
 
 module.exports = app;
