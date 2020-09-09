daf4eb1e9a538e318e38852d85ebd08e4288ff11
kk <kk>
2020-09-09 10:42:38 +0430

    test tables for database

---
+++ sql.sql
@@ -0,0 +1,16 @@
+create database gardeshgari_db;
+use gardeshgari_db;
+
+create table users_tbl(
+    user_id int NOT NULL AUTO_INCREMENT,
+    username varchar(50),
+    userfamily varchar(50),
+    phonenumber varchar(11) not null,
+    national_id_number varchar(10) not null,
+    PRIMARY KEY (user_id)
+);
+
+create table registerycode_tbl(
+	phonenumber varchar(11),
+    code varchar(5)
+)
