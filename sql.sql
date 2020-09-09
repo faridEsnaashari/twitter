61b1266437f724fea6a31be4ea4f48042e6fc7a6
kk <kk>
2020-09-09 12:01:01 +0430

    edit users_tbl and add verified field

--- sql.sql
+++ sql.sql
@@ -3,10 +3,11 @@ use gardeshgari_db;
 
 create table users_tbl(
     user_id int NOT NULL AUTO_INCREMENT,
-    username varchar(50),
+    username varchar(50) DEFAULT 'user',
     userfamily varchar(50),
     phonenumber varchar(11) not null,
     national_id_number varchar(10) not null,
+    verified boolean DEFAULT false,
     PRIMARY KEY (user_id)
 );
 
