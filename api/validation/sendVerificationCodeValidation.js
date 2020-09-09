fb881bd87547673d059f0924ecf78ecf5c9242c3
kk <kk>
2020-09-09 11:18:49 +0430

    delete logs that are for debug

--- api/validation/sendVerificationCodeValidation.js
+++ api/validation/sendVerificationCodeValidation.js
@@ -2,11 +2,9 @@ const {isString, isUndefinedOrNull, isPhoneNumber} = require('../tools/tools');
 
 function checkValidation(req, res){
     if(isUndefinedOrNull(req.query.phonenumber)){
-        console.log("un");
         return false;
     }
     if(!isString(req.query.phonenumber)){
-        console.log("st");
         return false;
     }
     if(!isPhoneNumber(req.query.phonenumber)){
