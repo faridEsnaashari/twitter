8345ad7c4af1b13b16033dda29a563fce1ded7ee
kk <kk>
2020-09-09 10:42:10 +0430

    use generateRandomCode to generate random code

--- api/randomCode.js
+++ api/randomCode.js
@@ -1,2 +1,4 @@
-const rand = Math.floor(Math.floor(10000 + Math.random() * 90000)) + 1;
-module.exports = rand;
+function generateRandomCode(){
+    return Math.floor(Math.floor(10000 + Math.random() * 90000)) + 1;
+}
+module.exports = generateRandomCode;
