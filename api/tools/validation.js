f6600a6c61b010abba2f4f044a9ba785a55c0591
kk <kk>
2020-09-10 12:52:12 +0430

    add IsIranMobile and IsIranNationalCode and some change

--- api/tools/validation.js
+++ api/tools/validation.js
@@ -1,4 +1,4 @@
-function isString(value){
+function IsString(value){
     if(typeof value === "string"){
         return true;
     }
@@ -7,7 +7,7 @@ function isString(value){
     }
 }
 
-function isUndefinedOrNull(value){
+function IsUndefinedOrNull(value){
     if(!value){
         return true;
     }
@@ -16,7 +16,7 @@ function isUndefinedOrNull(value){
     }
 }
 
-function isPhoneNumber(value){
+function containAlphabetical(value){
     const reg = new RegExp('^[0-9]+$');
     if(value.length === 11 && reg.test(value)){
         return true;
@@ -26,7 +26,34 @@ function isPhoneNumber(value){
     }
 }
 
+function IsIranMobile(Data){
+    const pattern = new RegExp(/^(?:98|\+98|0098|0)?9[0-9]{9}$/);
+    if(pattern.test(String(Data).toLowerCase())){
+        return true;
+    } 
+    else {
+        return false;
+    }
+}
 
-module.exports.isString = isString;
-module.exports.isUndefinedOrNull = isUndefinedOrNull;
-module.exports.isPhoneNumber = isPhoneNumber;
+function IsIranNationalCode(Data){
+    const pattern = new RegExp(/^\d{10}$/);
+    if (!pattern.test(Data)){
+        return false;
+    }
+    var check = +Data[9];
+    var sum = Array(9).fill().map((_, i) => +Data[i] * (10 - i)).reduce((x, y) => x + y) % 11;
+    if((sum < 2 && check == sum) || (sum >= 2 && check + sum == 11)){
+        return true;
+    } 
+    else {
+        return false;
+    }
+}
+
+
+module.exports.IsString = IsString;
+module.exports.IsUndefinedOrNull = IsUndefinedOrNull;
+module.exports.containAlphabetical = containAlphabetical;
+module.exports.IsIranMobile = IsIranMobile;
+module.exports.IsIranNationalCode = IsIranNationalCode;
