f35e4b7649e822df69652c28af97575a12c86133
kk <kk>
2020-09-10 08:59:45 +0430

    add unique constraint to some fields

--- sql.sql
+++ sql.sql
@@ -5,13 +5,13 @@ create table users_tbl(
     user_id int NOT NULL AUTO_INCREMENT,
     username varchar(50) DEFAULT 'user',
     userfamily varchar(50),
-    phonenumber varchar(11) not null,
-    national_id_number varchar(10) not null,
+    phonenumber varchar(11) not null UNIQUE,
+    national_id_number varchar(10) not null UNIQUE,
     verified boolean DEFAULT false,
     PRIMARY KEY (user_id)
 );
 
 create table registerycode_tbl(
-	phonenumber varchar(11),
+	phonenumber varchar(11) UNIQUE,
     code varchar(5)
 )
