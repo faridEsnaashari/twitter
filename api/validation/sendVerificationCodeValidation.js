20174fec9751b598ead68aecc3cb30a8969a644f
kk <kk>
2020-09-10 09:07:25 +0430

    also check code parameter

--- api/validation/sendVerificationCodeValidation.js
+++ api/validation/sendVerificationCodeValidation.js
@@ -10,6 +10,12 @@ function checkValidation(req, res){
     if(!isPhoneNumber(req.query.phonenumber)){
         return false;
     }
+    if(isUndefinedOrNull(req.query.code)){
+        return false;
+    }
+    if(!isString(req.query.code)){
+        return false;
+    }
     return true;
 }
 
