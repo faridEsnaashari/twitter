b9a1830f8240f05d41d2c05f57a79f486240b62b
kk <kk>
2020-09-10 09:07:58 +0430

    add register route

---
+++ api/routes/register.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+const postController = require('../controllers/register/post');
+const checkValidation = require('../validation/registerValidation');
+
+router.post('/', (req, res, next) => {
+    if(checkValidation(req, res)){
+        postController(req, res);
+    }
+    else{
+        responseJson = {
+            message: "bad parameter provided"
+        };
+
+        return res.status(400).json(responseJson);
+    }
+});
+
+module.exports = router;
