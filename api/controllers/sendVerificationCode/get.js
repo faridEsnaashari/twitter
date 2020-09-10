e396592abe0f87798d0e79d2f4edc6a298289103
kk <kk>
2020-09-10 09:04:14 +0430

    sendverificationcode just send sms and do nothing else

--- api/controllers/sendVerificationCode/get.js
+++ api/controllers/sendVerificationCode/get.js
@@ -1,55 +1,31 @@
-const {Token,VerificationCode} = require('sms-ir');
-const {APIKEY} = require('../../config');
-const {SECRETKEY} = require('../../config');
-const generateRandomCode = require('../../tools/randomCode');
-const connection = require('../../tools/connection');
+const { Token, VerificationCode } = require('sms-ir');
+const { APIKEY } = require('../../config');
+const { SECRETKEY } = require('../../config');
 
 const token = new Token();
 const verificationCode = new VerificationCode();
 
-async function get(req, res){
+async function get(req, res) {
     const phonenumber = req.query.phonenumber;
-    const code = generateRandomCode();
-    const userExistsQuery = `select * from users_tbl where phonenumber = ${ phonenumber }`;
-    connection.query(userExistsQuery, (err, result, fields) => {
-        if(err){
-            console.error(err);
-            return;
-        }
-        
-        if(result.length === 0){
-            const insertIntoRegestryQuery = `insert into registerycode_tbl value('${ phonenumber }', '${ code }')`
-            connection.query(insertIntoRegestryQuery, async(err, result, fields) => {
-                if(err){
-                    console.error(err);
-                    return;
-                }
-                try{
-                    const tokenResult = await token.get(APIKEY, SECRETKEY);
-                    const verificationResult = await verificationCode.send(tokenResult, phonenumber, code);
-                    if(verificationResult.IsSuccessful){
-                        const responseJson = {
-                            message : "verification code sent"
-                        };
-                        return res.status(200).json(responseJson);
-                    }
-                    else{
-                        throw(new Error('sms panel error'));
-                    }
-                }
-                catch(err){
-                    console.error(err);
-                }
-            });
-        }
-        else{
+    const code = req.query.code;
+
+    try {
+        const tokenResult = await token.get(APIKEY, SECRETKEY);
+        const verificationResult = await verificationCode.send(tokenResult, phonenumber, code);
+        if (verificationResult.IsSuccessful) {
             const responseJson = {
-                message : "user existed already"
+                message: "verification code sent"
             };
-            return res.status(409).json(responseJson);
+            return res.status(200).json(responseJson);
         }
-    });
-    
+        else {
+            throw (new Error('sms panel error'));
+        }
+    }
+    catch (err) {
+        console.error(err);
+    }
+
 }
 
 module.exports = get;
