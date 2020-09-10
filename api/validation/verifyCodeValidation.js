eac0d57eb523082d853e786884e6488ee279911b
kk <kk>
2020-09-10 09:12:04 +0430

    add validations stuff for verifycode route

---
+++ api/validation/verifyCodeValidation.js
@@ -0,0 +1,22 @@
+const {isString, isUndefinedOrNull, isPhoneNumber} = require('../tools/tools');
+
+function checkValidation(req, res){
+    if(isUndefinedOrNull(req.query.phonenumber)){
+        return false;
+    }
+    if(!isString(req.query.phonenumber)){
+        return false;
+    }
+    if(!isPhoneNumber(req.query.phonenumber)){
+        return false;
+    }
+    if(isUndefinedOrNull(req.query.code)){
+        return false;
+    }
+    if(!isString(req.query.code)){
+        return false;
+    }
+    return true;
+}
+
+module.exports = checkValidation;
