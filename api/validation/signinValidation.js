3cfa754b792706f0e715fad4c99e6feac59a5722
kk <kk>
2020-09-10 09:12:24 +0430

    add validations stuff for signin route

---
+++ api/validation/signinValidation.js
@@ -0,0 +1,16 @@
+const {isString, isUndefinedOrNull, isPhoneNumber} = require('../tools/tools');
+
+function checkValidation(req, res){
+    if(isUndefinedOrNull(req.query.phonenumber)){
+        return false;
+    }
+    if(!isString(req.query.phonenumber)){
+        return false;
+    }
+    if(!isPhoneNumber(req.query.phonenumber)){
+        return false;
+    }
+    return true;
+}
+
+module.exports = checkValidation;
