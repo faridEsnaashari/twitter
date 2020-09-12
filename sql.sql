893419623117232637e88d643f5de52313925e0c
kk <kk>
2020-09-12 12:13:17 +0430

    add twitts_tbl retwitters_tbl likes_tbl replays_to_twitt_tbl tables

--- sql.sql
+++ sql.sql
@@ -14,4 +14,37 @@ create table users_tbl(
 create table registerycode_tbl(
 	phonenumber varchar(11) UNIQUE,
     code varchar(5)
-)
+);
+
+create table twitts_tbl(
+	twitt_id int AUTO_INCREMENT,
+    text Text(300) not null,
+    img_link char(100),
+    user_id int not null,
+    date char(24) not null,
+    replay_to_id int,
+    PRIMARY KEY(twitt_id),
+    FOREIGN KEY (user_id) REFERENCES users_tbl(user_id),
+    FOREIGN KEY (replay_to_id) REFERENCES twitts_tbl(twitt_id)
+);
+
+create table retwitters_tbl(
+	user_id int not null,
+    twitt_id int not null,
+    FOREIGN KEY (user_id) REFERENCES users_tbl(user_id),
+    FOREIGN KEY (twitt_id) REFERENCES twitts_tbl(twitt_id)
+);
+
+create table likes_tbl(
+	user_id int not null,
+    twitt_id int not null,
+    FOREIGN KEY (user_id) REFERENCES users_tbl(user_id),
+    FOREIGN KEY (twitt_id) REFERENCES twitts_tbl(twitt_id)
+);
+
+create table replays_to_twitt_tbl(
+	twitt_id int not null,
+    twitt_replay_id int not null,
+    FOREIGN KEY (twitt_id) REFERENCES twitts_tbl(twitt_id),
+    FOREIGN KEY (twitt_replay_id) REFERENCES twitts_tbl(twitt_id)
+);
