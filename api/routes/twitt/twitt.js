c9c2b5e7f123962b59136883476f1225db9960b4
kk <kk>
2020-09-13 11:55:09 +0430

    add a middleware for /twitt/deletetwitt route

--- api/routes/twitt/twitt.js
+++ api/routes/twitt/twitt.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const sendtwitt = require('./sendtwitt');
 const gettwitt = require('./gettwitt');
 const getusertwitts = require('./getusertwitts');
+const deletetwitt = require('./deletetwitt');
 
 router.use('/sendtwitt', sendtwitt);
 router.use('/gettwitt', gettwitt);
 router.use('/getusertwitts', getusertwitts);
+router.use('/deletetwitt', deletetwitt);
 
 module.exports = router;
