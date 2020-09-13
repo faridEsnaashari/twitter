b69072f04c18ddb60085960ecdafe15e6f3fd6f6
kk <kk>
2020-09-13 10:27:06 +0430

    add middleware for /twitt/gettwitt route

--- api/routes/twitt/twitt.js
+++ api/routes/twitt/twitt.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const sendtwitt = require('./sendtwitt');
+const gettwitt = require('./gettwitt');
+
 router.use('/sendtwitt', sendtwitt);
+router.use('/gettwitt', gettwitt);
 
 module.exports = router;
