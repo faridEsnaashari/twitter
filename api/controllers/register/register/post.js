863e5c2899eb5d4529bcee3307b4e397a0a4e04d
kk <kk>
2020-09-14 11:00:07 +0430

    change the flow of whole register processes system. /register route now do the blow action: 1- get log_id from signin_token
    2- if log_id exist in database and it has a verified phonenumber, then
    get that phonenumber
    
    3- check phonenumber or naitinal_id_number doesn't exist in users_tbl
    
    4- insert users information into users_tbl

--- api/controllers/register/register/post.js
+++ api/controllers/register/register/post.js
@@ -1,60 +1,80 @@
 const { connection } = require('../../../tools/connectionManager');
 const { executeQuery } = require('../../../tools/connectionManager');
 const fetch = require('node-fetch');
-const generateRandomCode = require('../../../tools/randomCode');
+const token = require('../../../tools/token');
 
 async function post(req, res) {
-    const username = req.body.username;
-    const userfamily = req.body.userfamily;
-    const phonenumber = req.body.phonenumber;
-    const national_id_number = req.body.national_id_number;
-
-    const code = generateRandomCode();
-
     try {
-        let query = `insert into users_tbl(username, userfamily, phonenumber, national_id_number) value('${username}', '${userfamily}', '${phonenumber}', '${national_id_number}')`;
+        const username = req.body.username;
+        const userfamily = req.body.userfamily;
+        const national_id_number = req.body.national_id_number;
+
+        const signin_token = req.body.signin_token;
+        const log_id = token.verify(signin_token);
+
+        let query = `select * from verification_log_tbl where log_id = ${ log_id }`;
+        const selectFromVerificationLogTBLResult = await executeQuery(connection, query);
+        if(!selectFromVerificationLogTBLResult[0].verified){
+            throw "invalid singin_token";
+        }
+        const phonenumber = selectFromVerificationLogTBLResult[0].phonenumber;
+
+        query = `select * from users_tbl where national_id_number = '${national_id_number}'`;
+        const selectNationalIdFromUsersTBLResult = await executeQuery(connection, query);
+
+        if (selectNationalIdFromUsersTBLResult.length !== 0) {
+            throw "a user already exist with this national_id_number";
+        }
+
+        query = `select * from users_tbl where phonenumber = '${phonenumber}'`;
+        const selectPhonenumberFromUsersTBLResult = await executeQuery(connection, query);
+
+        if (selectPhonenumberFromUsersTBLResult.length !== 0) {
+            throw "a user already exist with this phonenumber";
+        }
+
+        query = `insert into users_tbl(username, userfamily, phonenumber, national_id_number) value('${username}', '${userfamily}', '${phonenumber}', '${national_id_number}')`;
         const insertResult = await executeQuery(connection, query);
-        const fetchResult = await fetch(`http://localhost:3000/sendvrificationcode?phonenumber=${phonenumber}&code=${code}`);
-        if (fetchResult.status === 200) {
-            query = `delete from registerycode_tbl where phonenumber = ${phonenumber}`;
-            const deleteResult = await executeQuery(connection, query);
-            query = `insert into registerycode_tbl value('${phonenumber}', '${code}')`;
-            const insertToRegestryCodeTBLResult = await executeQuery(connection, query);
-            const responseJson = {
-                status: 201,
-                success: true,
-                message: "user created",
-            };
-            return res.status(201).json(responseJson);
-        }
-        else {
-            const error = {
-                status: 503,
-                success: false,
-                message: "sms panel error",
-            };
-            return res.status(503).json(error);
-        }
+
+        const responseJson = {
+            status: 201,
+            success: true,
+            message: "user created",
+        };
+        return res.status(201).json(responseJson);
     }
     catch (err) {
         console.error(err);
-        if (err.code === 'ER_DUP_ENTRY') {
-            const responseJson = {
+        if((err.message && (err.message.includes('jwt') || err.message.includes('invalid signature'))) || err === "invalid singin_token" || err.message === "invalid token"){
+            const error = {
+                status: 403,
+                success: false,
+                message: "invalid singin_token",
+            };
+            return res.status(403).json(error);
+        }
+        if(err === "a user already exist with this national_id_number"){
+            const error = {
                 status: 409,
                 success: false,
-                message: "user already existed",
+                message: "a user already existed with this national_id_number",
             };
-
-            return res.status(409).json(responseJson);
+            return res.status(409).json(error);
         }
-        else{
+        if(err === "a user already exist with this phonenumber"){
             const error = {
-                status: 500,
+                status: 409,
                 success: false,
-                message: "internal server error",
+                message: "a user already existed with this phonenumber",
             };
-            return res.status(500).json(error);
+            return res.status(409).json(error);
         }
+        const error = {
+            status: 500,
+            success: false,
+            message: "internal server error",
+        };
+        return res.status(500).json(error);
     }
 }
 
