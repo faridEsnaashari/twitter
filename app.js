c7b8856a016eed065ce014df22227cac28954202
kk <kk>
2020-09-14 10:50:37 +0430

    add express-device module and config it

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const {connection} = require('./api/tools/connectionManager');
 const bodyParser = require('body-parser');
+var device = require('express-device');
 
 const app = express();
 
 app.use(bodyParser.json())
+app.use(device.capture());
 
 const signinRoute = require('./api/routes/register/signin');
-const sendVerificationCodeRoute = require('./api/routes/register/sendVerificationCode');
 const verifyCodeRoute = require('./api/routes/register/verifyCode');
 const registerRoute = require('./api/routes/register/register');
+const verifyphonenumberRoute = require('./api/routes/register/verifyphonenumber');
 const twittRotue = require('./api/routes/twitt/twitt');
 
 app.use('/signin', signinRoute);
-app.use('/sendvrificationcode', sendVerificationCodeRoute);
 app.use('/verifycode', verifyCodeRoute);
 app.use('/register', registerRoute);
+app.use('/verifyphonenumber', verifyphonenumberRoute);
 app.use('/twitt', twittRotue);
 
 app.use((req, res) => {
