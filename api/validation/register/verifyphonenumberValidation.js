8e447f99e7de5b05d8972b3730cbb8b6267c3ce1
kk <kk>
2020-09-14 11:17:10 +0430

    validation for /verifyphonenumber route

---
+++ api/validation/register/verifyphonenumberValidation.js
@@ -0,0 +1,30 @@
+const {IsString, IsUndefinedOrNull, IsIranMobile} = require('../../tools/validation');
+
+function checkValidation(req, res){
+    if(IsUndefinedOrNull(req.query.phonenumber)){
+        const error = {
+            message: 'phonenumber null or undefined',
+            field: 'phonenumber',
+            error: 'undefined'
+        }
+        throw error;
+    }
+    if(!IsString(req.query.phonenumber)){
+        const error = {
+            message: 'phonenumber should be string',
+            field: 'phonenumber',
+            error: 'wrong format'
+        }
+        throw error;
+    }
+    if(!IsIranMobile(req.query.phonenumber)){
+        const error = {
+            message: 'the format of phonenumber is not a iran phonenumber',
+            field: 'phonenumber',
+            error: 'not iran phonenumber'
+        }
+        throw error;
+    }
+}
+
+module.exports = checkValidation;
