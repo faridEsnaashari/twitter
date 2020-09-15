059281d9b809736e2c801eff473e409ae5ea1c17
kk <kk>
2020-09-15 10:05:39 +0430

    use environment variable calls PORT to set port of server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const app = require('./app');
+const env = require('./config');
 
 const server = http.createServer(app);
 
-server.listen(3000);
+server.listen(env.PORT);
