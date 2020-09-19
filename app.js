670b45d3a7dbd8d48b39608df80526f49259b8cc
kk <kk>
2020-09-19 09:36:27 +0430

    routes no longer process here. app.js just check if the url search for v1 or v2 and direct request to v1.js file in api/v1

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 global.tools = require('./config').path.tools;
 global.env = require('./config').env;
-global.controllers = require('./config').path.controllers;
-global.validations = require('./config').path.validations;
+global.controllers = require('./config').path.v1.controllers;
+global.validations = require('./config').path.v1.validations;
 
 const express = require('express');
 const bodyParser = require('body-parser');
 var device = require('express-device');
 const responseController = require(global.tools.responser);
+const v1 = require('./api/v1/v1');
 
 const app = express();
 
@@ -14,17 +15,8 @@ app.use(bodyParser.json());
 app.use(device.capture());
 app.use(responseController());
 
-const signinRoute = require('./api/routes/register/signin');
-const verifyCodeRoute = require('./api/routes/register/verifyCode');
-const registerRoute = require('./api/routes/register/register');
-const verifyphonenumberRoute = require('./api/routes/register/verifyphonenumber');
-const twittRotue = require('./api/routes/twitt/twitt');
 
-app.use('/signin', signinRoute);
-app.use('/verifycode', verifyCodeRoute);
-app.use('/register', registerRoute);
-app.use('/verifyphonenumber', verifyphonenumberRoute);
-app.use('/twitt', twittRotue);
+app.use('/v1', v1);
 
 app.use((req, res) => {
     return res.responseController.error(404, "wrong route or method");
