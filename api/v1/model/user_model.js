6882a4be88213247b66cf4274bb091650a6c54e7
farid <faride13578642@gmail.com>
2020-09-23 10:14:07 +0330

    create user schema and model

---
+++ api/v1/model/user_model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const phonenumber_detail = {
+    type: String,
+    required: true,
+    unique: true,
+};
+
+const national_id_number_detail = {
+    type: String,
+    required: true,
+    unique: true,
+}
+
+const username_detail = {
+    type: String,
+    default: ""
+}
+
+const userfamily_detail = {
+    type: String,
+    default: ""
+}
+
+const user_schema = new mongoose.Schema({
+    username: username_detail,
+    userfamily: userfamily_detail,
+    phonenumber: phonenumber_detail,
+    national_id_number: national_id_number_detail,
+});
+
+module.exports = mongoose.model('User', user_schema, 'users');
