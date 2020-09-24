42cee295f38062e8b1e868f704db9386edf5d2fa
farid <faride13578642@gmail.com>
2020-09-24 11:06:32 +0200

    comment "getusertwitts" route. with mongo, no more need to this route.

--- api/v1/routes/twitt/twitt.js
+++ api/v1/routes/twitt/twitt.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 const sendtwitt = require('./sendtwitt');
 const gettwitt = require('./gettwitt');
-const getusertwitts = require('./getusertwitts');
+//const getusertwitts = require('./getusertwitts');
 const deletetwitt = require('./deletetwitt');
 
 router.use('/sendtwitt', sendtwitt);
 router.use('/gettwitt', gettwitt);
-router.use('/getusertwitts', getusertwitts);
+//router.use('/getusertwitts', getusertwitts);
 router.use('/deletetwitt', deletetwitt);
 
 module.exports = router;
