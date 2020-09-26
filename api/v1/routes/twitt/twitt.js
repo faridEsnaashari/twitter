430a9205a64cf76f0764d28c09e5503e09f91d57
farid <faride13578642@gmail.com>
2020-09-26 14:46:22 +0330

    add like router for /twitt/like route

--- api/v1/routes/twitt/twitt.js
+++ api/v1/routes/twitt/twitt.js
@@ -6,11 +6,13 @@ const gettwitt = require('./gettwitt');
 //const getusertwitts = require('./getusertwitts');
 const deletetwitt = require('./deletetwitt');
 const retwitt = require('./retwitt');
+const like = require('./like');
 
 router.use('/sendtwitt', sendtwitt);
 router.use('/gettwitt', gettwitt);
 //router.use('/getusertwitts', getusertwitts);
 router.use('/deletetwitt', deletetwitt);
 router.use('/retwitt', retwitt);
+router.use('/like', like);
 
 module.exports = router;
