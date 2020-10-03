14f050a4cfe0ccc728473327c34e3bb1f5dc6ad1
farid <faride13578642@gmail.com>
2020-10-03 12:09:29 +0200

    add pay route for payment stuff

--- api/v1/v1.js
+++ api/v1/v1.js
@@ -6,11 +6,13 @@ const verifyCodeRoute = require('./routes/register/verifyCode');
 const registerRoute = require('./routes/register/register');
 const verifyphonenumberRoute = require('./routes/register/verifyphonenumber');
 const twittRotue = require('./routes/twitt/twitt');
+const payRoute = require('./routes/pay/pay');
 
 router.use('/signin', signinRoute);
 router.use('/verifycode', verifyCodeRoute);
 router.use('/register', registerRoute);
 router.use('/verifyphonenumber', verifyphonenumberRoute);
 router.use('/twitt', twittRotue);
+router.use('/pay', payRoute);
 
 module.exports = router;
